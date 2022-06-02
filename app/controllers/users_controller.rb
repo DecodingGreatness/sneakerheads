17e978996b333aeb5a7f6c40aa4426b7216c422d
Edward Ho <pythoned77@gmail.com>
2022-06-02 14:25:55 +0800

    Individual user page created with a new sneaker function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+  end
+
+  # def edit
+  # end
+
+  # def update
+  #   @user.update(user_params)
+  # end
+
+  # def destroy
+  #   @user.destroy
+  # end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # def user_params
+  #   params.require(:user).permit(:id)
+  # end
 end
