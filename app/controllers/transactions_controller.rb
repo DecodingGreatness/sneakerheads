fb4aae4953e13f1e0a70257781e546fc2e70fcac
Huiling Tan <huilingt@hotmail.com>
2022-06-04 21:44:12 +0800

    added datetime to transactions migration file, controller and transaction details page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,7 +7,7 @@ class TransactionsController < ApplicationController
     @buyer = @transaction.buyer
     @seller = @transaction.seller
     @sneaker.user = @transaction.buyer
-  end
+    end
 
   def new
     @transaction = Transaction.new
@@ -16,6 +16,7 @@ class TransactionsController < ApplicationController
 
   def create
     @transaction = Transaction.new(transaction_params)
+    @transaction.date = DateTime.now
     @transaction.save!
 
     @sneaker = @transaction.sneaker
